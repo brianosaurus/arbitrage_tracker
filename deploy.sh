72a58e6742d4ea891d4e2fda4ea6fe9eb5efb990
Brian Woods <me@brian.biz>
2026-03-03 21:14:15 -0800

    running for really long (5 days)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tar czf - --no-xattrs *.py | ssh frankfurt "cd arbitrage_tracker && tar xzf -"
-ssh frankfurt "cd arbitrage_tracker; source venv/bin/activate; python3 tracker.py --follow --duration 10" | tee /tmp/arbitrage_tracker.log
+ssh frankfurt "cd arbitrage_tracker; source venv/bin/activate; python3 tracker.py --follow --duration 8640" | tee /tmp/arbitrage_tracker.log
